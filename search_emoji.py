4d86a7bde9dfafb9c281ad2f39318dda9a261926
Esteban Quiroz <equirozh@Estebans-MacBook-Air.local>
2025-01-07 10:16:04 +0100

    Delete useless file

--- search_emoji.py
+++
@@ -1,9 +0,0 @@
-import re
-import emoji
-from emoji import EMOJI_DATA
-
-token = 'pizza'
-
-for emoji_char, data in EMOJI_DATA.items():
-    if data.get("en") == f':{token}:' or data.get("alias") == token:
-        print(f"Emoji: {emoji_char}, Alias: {alias}, Description: {search_term}")
